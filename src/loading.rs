ac9384e2c58812976f0335000931c734ea96f378
John Swift <johnswift@live.com>
2026-01-11 18:35:12 +0000

    Add: dirty and quick file upload protocol

--- src/loading.rs
+++ src/loading.rs
@@ -1,24 +1,20 @@
-use std::ffi::OsString;
 use std::fs::File;
 use std::path::Path;
 use zstd;
-
-use std::io::{Error, ErrorKind};
 
 pub struct CompressedFile {
     bytes: Vec<u8>,
     segments: usize,
     last_segment_size: usize,
-    name: OsString,
+    name: String,
 }
 
 impl CompressedFile {
-    pub fn from_path<T: AsRef<Path>>(path: T, segments: usize) -> std::io::Result<Self> {
-        let name = path
-            .as_ref()
-            .file_name()
-            .ok_or(Error::new(ErrorKind::NotFound, "No file name in path"))?
-            .to_owned();
+    pub fn from_path<T: AsRef<Path>>(
+        path: T,
+        segments: usize,
+        name: String,
+    ) -> std::io::Result<Self> {
         let file: File = File::open(path)?;
         let bytes = zstd::encode_all(file, 3)?;
         let size = bytes.len();
@@ -30,18 +26,49 @@ impl CompressedFile {
         })
     }
 
-    pub fn from_file<T: Into<OsString>>(
-        file: File,
-        segments: usize,
-        name: T,
-    ) -> std::io::Result<Self> {
+    pub fn from_file(file: File, segments: usize, name: String) -> std::io::Result<Self> {
         let bytes = zstd::encode_all(file, 3)?;
         let size = bytes.len();
         Ok(Self {
             bytes,
             segments,
             last_segment_size: size & segments,
-            name: name.into(),
+            name,
         })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        &self.bytes
+    }
+
+    pub fn chunks(&self) -> u32 {
+        self.segments as u32
+    }
+
+    pub fn chunk_size(&self) -> u32 {
+        self.bytes.len() as u32 / self.segments as u32
+    }
+
+    pub fn last_chunk_size(&self) -> u32 {
+        self.last_segment_size as u32
+    }
+
+    pub fn get_chunk(&'static self, chunk: u32) -> Option<&'static [u8]> {
+        if chunk as usize > self.segments {
+            return None;
+        }
+
+        if chunk as usize == self.segments {
+            return Some(&self.bytes[(self.bytes.len() - self.last_segment_size - 1)..]);
+        }
+
+        let left = self.bytes.len() / self.segments * chunk as usize;
+        let right = left + (self.bytes.len() / self.segments);
+
+        Some(&self.bytes[left..right])
     }
 }
--- src/upload.rs
+++ src/upload.rs
@@ -1,4 +1,9 @@
-use crate::handler::{File, Files, Stream};
+use std::ffi::OsString;
+
+use crate::{
+    handler::{File, Files, Stream},
+    loading::CompressedFile,
+};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub async fn upload_file(Stream(mut stream): Stream, Files(files): Files) {
@@ -7,18 +12,46 @@ pub async fn upload_file(Stream(mut stream): Stream, Files(files): Files) {
         return;
     }
 
-    let file_name: String;
+    let mut terminator: usize = 255;
     for (i, b) in file_name_bytes.iter().enumerate() {
-        if *b == 0 || i == 255 {
-            file_name = 
+        if *b == 0 {
+            terminator = i;
+            break;
         }
     }
 
+    let file_name = String::from_utf8_lossy(&file_name_bytes[..terminator]);
+
+    let mut file: Option<&'static CompressedFile> = None;
+
+    for f in files {
+        if f.name() == file_name {
+            file = Some(f);
+            break;
+        }
+    }
+
+    if file.is_none() {
+        // handle some no named file case here
+        todo!();
+    }
+
+    let file = file.unwrap();
+
+    stream.write_u32(file.chunks()).await.unwrap();
+    stream.write_u32(file.chunk_size()).await.unwrap();
+    stream.write_u32(file.last_chunk_size()).await.unwrap();
+
     loop {
-        let mut chunk = 0;
-        match stream.read_u16().await {
+        let chunk;
+        match stream.read_u32().await {
             Ok(m) => chunk = m,
             Err(_e) => return,
         }
+
+        stream
+            .write_all(file.get_chunk(chunk).unwrap())
+            .await
+            .unwrap();
     }
 }
